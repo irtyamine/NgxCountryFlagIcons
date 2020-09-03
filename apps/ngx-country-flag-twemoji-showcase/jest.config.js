3cf9a3f81841e683371850c6f16956474c6989d5
KingDarBoja <stevenbojato04@gmail.com>
2020-09-03 17:16:49 -0500

    initial commit

---
+++ apps/ngx-country-flag-twemoji-showcase/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  name: 'ngx-country-flag-twemoji-showcase',
+  preset: '../../jest.config.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: [
+        'jest-preset-angular/build/InlineFilesTransformer',
+        'jest-preset-angular/build/StripStylesTransformer',
+      ],
+    },
+  },
+  coverageDirectory: '../../coverage/apps/ngx-country-flag-twemoji-showcase',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+};
