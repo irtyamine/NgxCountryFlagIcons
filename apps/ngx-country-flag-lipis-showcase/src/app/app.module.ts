5399a8db991e3f00a6e6a7e46d433bdba7897eef
KingDarBoja <stevenbojato04@gmail.com>
2020-09-09 17:21:56 -0500

    chore: add showcase app for lipis icons

---
+++ apps/ngx-country-flag-lipis-showcase/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {
+  NgxCountryFlagIconsModule,
+  NgxCountryFlagIconsService,
+} from '@ngx-country-flag-icons/ngx-country-flag-icons';
+import { LipisIcons } from '@ngx-country-flag-icons/icons-data';
+
+import { AppRoutingModule } from './app.routing';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    NgxCountryFlagIconsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {
+  constructor(private countryFlagIconsService: NgxCountryFlagIconsService) {
+    this.countryFlagIconsService.registerIcons([
+      LipisIcons.countryFlagIconEs,
+    ]);
+  }
+}
