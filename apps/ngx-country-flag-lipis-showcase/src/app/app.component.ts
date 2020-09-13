97e8dc204ae86f28690e54868601628e11e79601
KingDarBoja <stevenbojato04@gmail.com>
2020-09-13 13:47:37 -0500

    misc: add africa country flags to africa module

--- apps/ngx-country-flag-lipis-showcase/src/app/app.component.ts
+++ apps/ngx-country-flag-lipis-showcase/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ngx-country-flag-icons-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ngx-country-flag-lipis-showcase';
